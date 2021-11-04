e590dd8d2cd569f8f488e833d4599eb99c4eca33
Ultra <ultramlg1108@gmail.com>
2021-11-03 18:32:45 -0700

    Fix potential bug

--- src/main/java/net/ultra03/noshield/mixin/client/MixinLivingEntity.java
+++ src/main/java/net/ultra03/noshield/mixin/client/MixinLivingEntity.java
@@ -1,6 +1,11 @@
 package net.ultra03.noshield.mixin.client;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
 import net.ultra03.noshield.client.NoShieldClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,11 +13,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-public class MixinLivingEntity {
+public abstract class MixinLivingEntity {
 
     @Inject(method = "Lnet/minecraft/entity/LivingEntity;stopUsingItem()V", at = @At("HEAD"))
     public void stopUsingItem(CallbackInfo ci) {
-        NoShieldClient.shouldRender = false;
+        if(((LivingEntity) (Object) this) instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) (Object) this;
+            if(player.getUuid().equals(MinecraftClient.getInstance().player.getUuid()))
+                NoShieldClient.shouldRender = false;
+        }
     }
 
 }
